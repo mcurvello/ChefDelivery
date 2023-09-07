8105a9771585c7764fbd3bc1664fdc9a252a3178
Marcio Curvello <marciocurvello@gmail.com>
2023-09-07 20:54:44 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Marcio Curvello on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
