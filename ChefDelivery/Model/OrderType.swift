6f189a46ea1f41c1eee66c4521ac5f3ca3fe413d
Marcio Curvello <marciocurvello@gmail.com>
2023-09-08 01:20:17 -0300

    Create OrderTypeView Component

---
+++ ChefDelivery/Model/OrderType.swift
@@ -0,0 +1,14 @@
+//
+//  OrderType.swift
+//  ChefDelivery
+//
+//  Created by Marcio Curvello on 08/09/23.
+//
+
+import Foundation
+
+struct OrderType: Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
