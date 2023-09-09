1bf6669171e7000cf765549992e07965468b1666
Marcio Curvello <marciocurvello@gmail.com>
2023-09-09 11:41:42 -0300

    add filter menu to stores

--- ChefDelivery/StoresView/StoresContainerView.swift
+++ ChefDelivery/StoresView/StoresContainerView.swift
@@ -10,17 +10,58 @@ import SwiftUI
 struct StoresContainerView: View {
     
     let title = "Lojas"
+    var filteredStores: [StoreType] {
+        return storesMock.filter {store in
+            store.stars >= ratingFilter
+        }
+    }
+    
+    @State private var ratingFilter = 0
+    
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title)
-                .font(.headline)
-            VStack(alignment: .leading, spacing: 30) {
-                ForEach(storesMock){store in
-                    NavigationLink {
-                        StoreDetailView(store: store)
+            HStack{
+                Text(title)
+                    .font(.headline)
+                Spacer()
+                Menu("Filtrar") {
+                    Button {
+                        ratingFilter = 0
                     } label: {
-                        StoreItemView(store: store)
+                        Text("Limpar filtro")
+                    }
+                    Divider()
+
+                    ForEach(1...5, id: \.self) {rating in
+                        Button {
+                            ratingFilter = rating
+                        } label: {
+                            if rating > 1 {
+                                Text("\(rating) estrelas ou mais")
+                            } else {
+                                Text("\(rating) estrela ou mais")
+                            }
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+            }
+            VStack(alignment: .leading, spacing: 30) {
+                if filteredStores.isEmpty {
+                    Text("Nenhum resultado encontrado.")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color("ColorRed"))
+                        .padding(.vertical, 32)
+                        .frame(maxWidth: .infinity)
+                } else {
+                    ForEach(filteredStores){store in
+                        NavigationLink {
+                            StoreDetailView(store: store)
+                        } label: {
+                            StoreItemView(store: store)
+                        }
                     }
                 }
             }
